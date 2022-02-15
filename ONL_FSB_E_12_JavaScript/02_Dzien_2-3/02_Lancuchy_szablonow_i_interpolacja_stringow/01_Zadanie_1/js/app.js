26fcc5b426ff64c366627b93cf9e9c36a2bdd7cb
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-15 01:29:52 +0100

    CLR_005_Zadanie_1

---
+++ ONL_FSB_E_12_JavaScript/02_Dzien_2-3/02_Lancuchy_szablonow_i_interpolacja_stringow/01_Zadanie_1/js/app.js
@@ -0,0 +1,9 @@
+// Zadanie 1 - rozwiązywane z wykładowcą
+// Stwórz dwie zmienne, które będą przechowywały dowolne liczby. Następnie wypisz ich sumę w konsoli korzystając z łańcucha szablonowego (Template strings). Przykład:
+//
+//     Suma dwóch liczb 2 i 4 to: 6
+
+const num1 = 2
+const num2 = 4
+
+console.log(`Suma dwóch liczb ${num1} i ${num2} to: ${num1 + num2}`)
