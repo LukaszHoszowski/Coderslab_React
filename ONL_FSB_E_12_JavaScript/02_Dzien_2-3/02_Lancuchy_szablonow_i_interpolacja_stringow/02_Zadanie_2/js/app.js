14da1c2da29b34e934f4a202582acae05b025500
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-15 01:32:44 +0100

    CLR_005_Zadanie_2

---
+++ ONL_FSB_E_12_JavaScript/02_Dzien_2-3/02_Lancuchy_szablonow_i_interpolacja_stringow/02_Zadanie_2/js/app.js
@@ -0,0 +1,9 @@
+// Zadanie 2
+// Wstaw do zmiennej swoje imię. Następnie wypisz je w konsoli razem z nazwiskiem. Przykład:
+//
+// Moje imię i nazwisko to: Chuck Norris
+
+const firstName = 'Lukasz'
+const lastName = 'Hoszowski'
+
+console.log(`Moje imię i nazwisko to: ${firstName + ' ' + lastName}`)
