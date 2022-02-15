af1091d545af3e80d693fa57670c0732a623c5a0
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-15 01:39:27 +0100

    CLR_006_Zadanie_1

---
+++ ONL_FSB_E_12_JavaScript/02_Dzien_2-3/03_Destrukturyzacja/01_Zadanie_1/js/app.js
@@ -0,0 +1,9 @@
+// Zadanie 1
+// Podstaw pod zmienne pierwszą i ostatnią wartość następującej tablicy:
+//
+//     ["snow", "rain", "sun"]
+// Wypisz zmienne w konsoli.
+
+let [a, , b] = ["snow", "rain", "sun"]
+
+console.log(a, b)
