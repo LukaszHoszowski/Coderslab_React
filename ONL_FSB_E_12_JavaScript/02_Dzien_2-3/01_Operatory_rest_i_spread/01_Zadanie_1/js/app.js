35706864ac7f0c746a36acce9b9d7c38338ccdc0
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-15 01:20:39 +0100

    CLR_004_Zadanie_1

---
+++ ONL_FSB_E_12_JavaScript/02_Dzien_2-3/01_Operatory_rest_i_spread/01_Zadanie_1/js/app.js
@@ -0,0 +1,8 @@
+// Zadanie 1
+// Stwórz zmienną przechowującą Twoje imię. Za pomocą operatora rozproszenia zapisz każdą literkę do tablicy.
+//
+//     Wypisz tablicę w konsoli.
+
+const name = 'Lukasz'
+
+console.log([...name])
